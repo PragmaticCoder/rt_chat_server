67c1380b19a59f16e738a7db4cc0aa55055670ad
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-02-21 06:28:26 -0600

    minor refactoring

--- src/router.js
+++ src/router.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Messages from '@/views/Messages'
 import TodoApp from '@/views/TodoApp'
-import ChatRoom from '@/views/ChatRoom.vue'
+import ChatRoom from '@/views/ChatRoom'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
     routes: [
