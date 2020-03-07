efd12b9257dafe5acdf5dc3ce828ad2592f78e42
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-07 14:22:09 -0600

    minor code refactoring

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 
-import store from '@/store' 
+import store from '@/store'
 import router from '@/router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // Vue.use(VueRouter)
 
 const vue = new Vue({
-  router,
-  store,
-  render: h => h(App)
-})
+    router,
+    store,
+    render: h => h(App)
+});
 
-vue.$mount('#app')
+vue.$mount('#app');
