75e3bef07a1e4f96600a93f2e42bc13143d66459
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-07 14:20:19 -0600

    renamed app from api to something meaningful

--- backend/chat/migrations/0002_chatmessage_thread.py
+++ backend/chat/migrations/0002_chatmessage_thread.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('api', '0001_initial'),
+        ('chat', '0001_initial'),
     ]
 
     operations = [
@@ -33,7 +33,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('message', models.TextField()),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
-                ('thread', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='api.Thread')),
+                ('thread', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='backend.chat.Thread')),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, verbose_name='sender')),
             ],
             options={
--- backend/urls.py
+++ backend/urls.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from .api.views import index_view, MessageViewSet
+from .chat.views import index_view, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register('messages', MessageViewSet)
