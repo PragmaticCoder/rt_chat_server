f3f7b9c0518295cab0bb575d4ee54ece9a16f345
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-07 14:38:45 -0600

    fixed the error in migration causing during renaming

--- backend/chat/migrations/0002_chatmessage_thread.py
+++ backend/chat/migrations/0002_chatmessage_thread.py
@@ -33,7 +33,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('message', models.TextField()),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
-                ('thread', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='backend.chat.Thread')),
+                ('thread', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='chat.Thread')),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, verbose_name='sender')),
             ],
             options={
