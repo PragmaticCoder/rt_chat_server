27ef4f18b1c97e4a45f50f8263687ebe060f8ca0
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-08 01:19:25 -0600

    created a new chat object

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 import App from '@/App.vue'
 
 import store from '@/store'
 import router from '@/router'
 
+import VueChatScroll from 'vue-chat-scroll'
+import BootstrapVue from 'bootstrap-vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(VueChatScroll)
+
 const vue = new Vue({
   router,
   store,
--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,13 +1,44 @@
 <template>
-   <h1>Chat Room</h1>
+  <b-container class="chat">
+    <h2 class="text-primary text-center">Real-Time Chat</h2>
+    <h5 class="text-secondary text-cen">Powered by Vue JS</h5>
+    <div class="card-body">
+      <!-- <p class="text-secondary nomessages" v-if="messages.length == 0">
+        [No messages yet!]
+      </p> -->
+      <div class="messages" v-chat-scroll="{ always: false, smooth: true }">
+        <div v-for="message in messages" :key="message.id">
+          <span class="text-info">[{{ message.name }}]</span>
+          <span>{{ message.message }}</span>
+          <span class="text-secondary time">{{ message.timestamp }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="create-action">
+      <CreateMessage :name="name" />
+    </div>
+  </b-container>
 </template>
 
 <script>
-    export default {
-        name: "ChatRoom"
-    }
+import CreateMessage from "@/components/CreateMessage";
+import moment from "moment";
+
+export default {
+  name: "ChatRoom",
+  props: ["name"],
+  components: {
+    CreateMessage
+  },
+  data() {
+    return {
+      messages: []
+    };
+  },
+//   created() {
+      
+//   }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
