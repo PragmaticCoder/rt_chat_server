c89df2f018ec0e669ab401171e87a494e416d455
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-07 20:07:14 -0600

    configured bootstreap so that we can use bootstrap in our template code

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from '@/App.vue'
 import store from '@/store' 
 import router from '@/router'
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 Vue.config.productionTip = false
 
 const vue = new Vue({
