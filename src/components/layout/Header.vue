679c9eb224a2309aa9544a7c71a3bb285a77ea21
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-03-07 23:47:54 -0600

    minor UI Tweaks

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <b-nav tabs align="center">
-      <b-nav-item to="/chatroom">Chat Room</b-nav-item>
-      <b-nav-item to="/todo">To Do App</b-nav-item>
-      <b-nav-item to="/messages">Message</b-nav-item>
+    <b-nav tabs align="center" danger>
+      <b-navbar toggleable="lg" type="dark" variant="warning">
+        <b-nav-item to="/chatroom">Chat Room</b-nav-item>
+        <b-nav-item to="/todo">To Do App</b-nav-item>
+        <b-nav-item to="/messages">Message</b-nav-item>
+      </b-navbar>
     </b-nav>
   </div>
 </template>
